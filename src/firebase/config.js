4fc35cda93eebab53ac4ae00af3562ac20c044d7
kakariko3 <kakariko01@gmail.com>
2021-05-19 21:58:28 +0900

    deploy firebase #10

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDlTt3VG6e2UuNv4Kf97hJ-ykm5_u7hANE",
+  authDomain: "chatbot-demo-6f71c.firebaseapp.com",
+  projectId: "chatbot-demo-6f71c",
+  storageBucket: "chatbot-demo-6f71c.appspot.com",
+  messagingSenderId: "1077763199923",
+  appId: "1:1077763199923:web:19f987f1c782c66cc4f1c6",
+  measurementId: "G-FPF9LS4CD8"
+};
+
+export default firebaseConfig;
